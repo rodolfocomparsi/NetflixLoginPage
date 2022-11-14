33635b6e3ac23228112ae3954983f27ab94f0349
rodolfo comparsi <rodolfocomparsi@hotmail.com>
2022-11-14 10:38:49 -0300

    Initial Commit

---
+++ NetflixLoginPage/NetflixLoginPageApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetflixLoginPageApp.swift
+//  NetflixLoginPage
+//
+//  Created by Rodolfo Comparsi on 14/11/22.
+//
+
+import SwiftUI
+
+@main
+struct NetflixLoginPageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
